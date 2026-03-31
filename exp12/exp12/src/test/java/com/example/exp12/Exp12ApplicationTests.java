ac8269a9a1710b15e11cf5c5540c7c8c7da1e1ed
TAMMANA VIJAY KUMAR <2400030069@kluniversity.in>
2026-03-31 11:23:58 +0530

    Add files via upload

---
+++ exp12/exp12/src/test/java/com/example/exp12/Exp12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exp12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exp12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
